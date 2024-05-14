d320d22dd468d802fb1773cd5ab52da346e38dd5
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2024-05-14 17:54:46 +0300

    @test

--- src/test/java/GetRequest.java
+++ src/test/java/GetRequest.java
@@ -1,12 +1,28 @@
 import io.restassured.RestAssured;
 
+import org.testng.annotations.Test;
+import io.restassured.matcher.RestAssuredMatchers;
+import org.hamcrest.Matchers;
+
 import static io.restassured.RestAssured.given;
 
+
 public class GetRequest {
+  /*
     public static void main(String[] args) {
+
         RestAssured
                 .given().baseUri("https://reqres.in/api/users")
                 .queryParam("page","1")
                 .when().get().prettyPrint();
+     }
+
+   */
+    @Test
+    public void Gettest(){
+      given()
+        .when()
+        .then();
     }
+
 }
