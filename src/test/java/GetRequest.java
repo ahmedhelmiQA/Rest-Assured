a8e6f1822c1674b31a468d792c7528d69737d193
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2024-05-15 17:58:59 +0300

    assert

--- src/test/java/GetRequest.java
+++ src/test/java/GetRequest.java
@@ -5,6 +5,9 @@ import io.restassured.matcher.RestAssuredMatchers;
 import org.hamcrest.Matchers;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.core.Is.is;
 
 
 public class GetRequest {
@@ -20,9 +23,13 @@ public class GetRequest {
    */
     @Test
     public void Gettest(){
-      given()
-        .when()
-        .then();
+      given().baseUri("https://664378116c6a656587073399.mockapi.io/api/v1")
+        .when().get("Users")
+        .then()
+       .log().all()
+       .assertThat().statusCode(200)
+       .assertThat().body("[0].name", is(equalTo("Courtney Oberbrunner")))
+       .assertThat().body(".name",hasItem("Mrs. Cindy Green"));
     }
 
 }
